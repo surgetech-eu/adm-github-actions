92f802075dccc47894d9a975b8805a5eb93fec69
kantipov <kantipov@gmail.com>
2020-08-06 14:40:27 +0100

    Add repo name to npm output

--- ci-tag-check-npm/index.js
+++ ci-tag-check-npm/index.js
@@ -2,3 +2,4 @@ const config = require(process.env.WORKSPACE + '/package.json')
 
 // Setting github workflow output
 console.log(`::set-output name=tag::${config.version}`)
+console.log(`::set-output name=repo::${config.name}`)
