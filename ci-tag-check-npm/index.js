e56a436c452970f18bccc1bce37d7df28c8a5294
kantipov <kantipov@gmail.com>
2020-08-26 16:21:01 +0100

    Update ci-tag-check-npm

--- ci-tag-check-npm/index.js
+++ ci-tag-check-npm/index.js
@@ -1,5 +1,9 @@
 const config = require(process.env.WORKSPACE + '/package.json')
 
-// Setting github workflow output
+// get short branch name
+var branch = process.env.GITHUB_REF.replace(/refs\/heads\//gm,'')
+
+// set github workflow output
 console.log(`::set-output name=tag::${config.version}`)
 console.log(`::set-output name=repo::${config.name}`)
+console.log(`::set-output name=branch::${branch}`)
