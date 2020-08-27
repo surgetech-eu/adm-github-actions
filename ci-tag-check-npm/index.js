57d10abc192eb922eecbf447772291d7a7d78680
kantipov <kantipov@gmail.com>
2020-08-27 16:57:18 +0100

    Add real tag check for npm

--- ci-tag-check-npm/index.js
+++ ci-tag-check-npm/index.js
@@ -1,4 +1,5 @@
 const config = require(process.env.GITHUB_WORKSPACE + '/package.json')
+const exec = require('child_process').exec;
 
 // get short branch name
 var branch = process.env.GITHUB_REF.replace(/refs\/heads\//gm,'')
@@ -7,3 +8,21 @@ var branch = process.env.GITHUB_REF.replace(/refs\/heads\//gm,'')
 console.log(`::set-output name=tag::${config.version}`)
 console.log(`::set-output name=repo::${config.name}`)
 console.log(`::set-output name=branch::${branch}`)
+
+// check tag
+var cmd = `npm view ${config.name} versions --json`
+exec(cmd, function(error, stdout, stderr) {
+    if (error) {
+        console.log(`::set-output name=message::internal error in tag checker`)
+        throw new Error(error.code, stderr);
+    }
+
+    let tags = JSON.parse(stdout);
+    for (let tag of tags){
+        if (tag == ${config.version}) {
+            console.log(`::set-output name=message::tag already exists`)
+            throw new Error("tag already exists");
+        }
+    }
+    console.log("tag is new, ok to continue");
+});
